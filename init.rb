54fa587e512e343331491e3de0b504417c98e09e
Stephane Moreau <smoreau@logikdev.com>
2013-02-14 15:45:00 -0700

    Add the URL of the github repository

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_documents_short do
   author 'Stephane Moreau'
   description 'A plugin which displays the documents page in a shorter way.'
   version '0.1.0'
-  #url 'http://example.com/path/to/plugin'
+  url 'https://github.com/smoreau/redmine_documents_short'
   author_url 'http://www.logikdev.com'
 end
