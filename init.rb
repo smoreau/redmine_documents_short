e3cc019bb466c6124a9349363e371fd04fa4a4e4
Stephane Moreau <smoreau@logikdev.com>
2013-02-14 15:36:55 -0700

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_documents_short do
+  name 'Documents Short plugin'
+  author 'Stephane Moreau'
+  description 'A plugin which displays the documents page in a shorter way.'
+  version '0.1.0'
+  #url 'http://example.com/path/to/plugin'
+  author_url 'http://www.logikdev.com'
+end
