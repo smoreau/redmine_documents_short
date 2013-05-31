27d3ee5fc0224e0b6829602f23e7c5dc6e903420
Stéphane Moreau <smoreau82@gmail.com>
2013-05-31 15:05:37 +0300

    Change the version number following the fix for Redmine 2.3

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_documents_short do
   name 'Documents Short plugin'
   author 'Stephane Moreau'
   description 'A plugin which displays the documents page in a shorter way.'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/smoreau/redmine_documents_short'
   author_url 'http://www.logikdev.com'
 end
