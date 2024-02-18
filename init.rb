11c69a8ea0a0dc9ab9a92561c1e25ebe16046730
Stephane Moreau <stephane.moreau@logikdev.com>
2024-02-18 15:34:00 +0000

    Update the plugin to get it compatible with Redmine 5.1.x

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_documents_short do
   name 'Documents Short plugin'
   author 'Stephane Moreau'
   description 'A plugin which displays the documents page in a shorter way.'
-  version '0.5.0'
+  version '0.6.0'
   url 'https://github.com/smoreau/redmine_documents_short'
   author_url 'http://www.logikdev.com'
 end
